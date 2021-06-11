7182cce140baf11780a799dcd6624da5ffcc1537
Qiyuan <qiyuan.chen2002@gmail.com>
2021-06-11 17:11:19 +0100

    Added many different fonts

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+//Getting the tmi module and setting it up
 const tmi = require('tmi.js');
 
 const options = {
@@ -16,44 +17,55 @@ const options = {
 };
 
 const client = new tmi.client(options);
-
 client.connect().catch(console.error);
 
-// client.on("connected", () => {
-//     setInterval(() => {
-//         client.action("failtowinbot", "Hello, this bot is now connected");
-//     }, 2000);
-// });
+//Takes a message and splits it into the command given and the actual text to change 
+const splitText = (message) => {
+	let command = message.split(" ")[0].toLowerCase();
+	let text = message.substring(command.length + 1, message.length);
+	return [command, text];
+}
 
-const convertText = (text) => {
+//Function to turn text into ascii letters
+const convertText = (text, type) => {
+
+	const fontNumbers = {
+		"bold": ["1D400", "1D41A", "1D7CE"],
+		"sans-serif": ["1D5A0", "1D5BA", "1D7E2"],
+		"sans-serif-bold": ["1D5D4", "1D5EE", "1D7EC"],
+		"monospace": ["1D670", "1D68A", "1D7F6"]
+	}
 
 	let changedText = "";
-
 	for (let i = 0; i < text.length; i++){
 
-		if ("abcdefghijklmnopqrstuvwxyz".includes(text[i])){
-			var codeForFont = 0x1D41A;
-			var subtract = 97;
-			var isLetter = true;
-		} else if ("ABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(text[i])){
-			var codeForFont = 0x1D400;
+		//Sets up info depending on type of data and the type of font to change it to
+		let notSymbol = true;
+		if ("ABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(text[i])){
+			var codeForFont = fontNumbers[type][0];
 			var subtract = 65;
-			var isLetter = true;
+		} else if ("abcdefghijklmnopqrstuvwxyz".includes(text[i])){
+			var codeForFont = fontNumbers[type][1];
+			var subtract = 97;
+		} else if ("0123456789".includes(text[i])){
+			var codeForFont = fontNumbers[type][2];
+			var subtract = 48;
 		} else {
-			var isLetter = false;
+			notSymbol = false;
 		}
 
-		if (isLetter){
+		if (notSymbol){
+
+			//Get ASCII of letter typed
 			let asciiCode = text.codePointAt(i);
-			console.log("ascii:", asciiCode);
 
-			let letterNumber = parseInt(codeForFont, 10) + (asciiCode - subtract);
-			console.log("letterNumber: ", letterNumber);
+			//Get unicode number for the corresponding letter
+			let letterNumber = parseInt(codeForFont, 16) + (asciiCode - subtract);
 
+			//Add unicode letter to the new string 
 			let unicodeOfLetter = "0x" + letterNumber.toString(16);
-			console.log("unicodeOfLetter: ", unicodeOfLetter);
-
 			changedText += String.fromCodePoint(unicodeOfLetter);
+
 		} else {
 			changedText += text[i];
 		}
@@ -61,15 +73,33 @@ const convertText = (text) => {
 	return changedText;
 }
 
+//Running the bot
 client.on("connected", () => console.log("connected"));
-
 client.on("message", (channel, user, message, self) => {
 	
 	if(self){ return };
 	
-	if(message.substring(0, 9).toLowerCase() === "!makebold"){
-		client.say(channel, convertText(message.substring(10, message.length)))
-	}
+	let command = splitText(message)[0];
+	let text = splitText(message)[1];
 
+	switch (command){
+		case "!commands":
+			client.say(channel, "!font1: bold text");
+			client.say(channel, "!font2: sans-serif text");
+			client.say(channel, "!font3: sans-serif bold text");
+			client.say(channel, "!font4: monospace text");
+			break;
+		case "!font1":
+			client.say(channel, convertText(text, "bold"));
+			break;
+		case "!font2":
+			client.say(channel, convertText(text, "sans-serif"));
+			break;
+		case "!font3":
+			client.say(channel, convertText(text, "sans-serif-bold"));
+			break;
+		case "!font4":
+			client.say(channel, convertText(text, "monospace"));
+			break;		
+	}	
 });
-
