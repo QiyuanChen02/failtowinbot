10115e6e77e9728f5953b8e4fb19ba40c54a47f7
Qiyuan <qiyuan.chen2002@gmail.com>
2021-06-26 14:00:02 +0100

    Put other channels on

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const options = {
 		username: process.env.TWITCH_BOT_USERNAME,
 		password: process.env.TWITCH_OAUTH_TOKEN
 	},
-	channels: ["failtowinbot"]
+	channels: ["failtowinbot", "failtowinpro", "chubbehmouse"]
 };
 
 const client = new tmi.client(options);
